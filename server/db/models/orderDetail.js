1035fc617486fd772bce37dec69d0393dd920374
Ani <aganita21@gmail.com>
2016-06-13 14:12:36 -0400

    fixed the navbar search and price change

--- server/db/models/orderDetail.js
+++ server/db/models/orderDetail.js
@@ -3,6 +3,7 @@
 var Sequelize = require('sequelize');
 var db = require('../_db');
 var math = require('math');
+var moment = require('moment');
 
 module.exports = function (db) {
   return db.define('order_detail', {
@@ -33,16 +34,14 @@ module.exports = function (db) {
     },
     hooks: {
       afterCreate: function (orderDet) {
-        //week ago
-        var endDate = new Date();
-        var startDate = new Date(endDate.setDate(endDate.getDate() - 7));
-
+        //get a date week ago
+        var startDate = moment().subtract(7, 'days').calendar();
         var Sock = db.model('sock');
 
         this.numberOfOrders(orderDet.sockId, startDate).then(function(numOfOrders){
           if ((+numOfOrders > 40 || +numOfOrders < 5) && orderDet.originalPrice > 3) {
             console.log("orderdet", orderDet.sockId)
-              Sock.increasePrice(orderDet.sockId, math.round((orderDet.originalPrice * 0.9),2))
+              Sock.changePrice(orderDet.sockId, math.round((orderDet.originalPrice * 0.9),2))
                 .then(function (res) {
                   console.log("hereree!!!!!!---->", res)
             })
