763299a87d68a6a76bb059dbc0df7ae4819ef72a
Michael Zhan <michaelzhan@Michaels-MacBook-Air.local>
2016-06-14 21:44:08 -0400

    uncommented aws feature after accidentally commenting it out

--- browser/js/design/design-directive.js
+++ browser/js/design/design-directive.js
@@ -44,40 +44,40 @@ app.directive('designView', function (SockFactory, $state, $http) {
           tags: tagsArr
         };
 
-        return SockFactory.saveDesign(newSockDataObj)
-        .then(function(result) {
-        	$state.go('user', {userId: result.data.userId})
-        })
+        // return SockFactory.saveDesign(newSockDataObj)
+        // .then(function(result) {
+        // 	$state.go('user', {userId: result.data.userId})
+        // })
 
-        // function dataURItoBlob(dataURI) {
-        //   var binary = atob(dataURI.split(',')[1]);
-        //   var array = [];
-        //   for(var i = 0; i < binary.length; i++) {
-        //     array.push(binary.charCodeAt(i));
-        //   }
-        //   return new Blob([new Uint8Array(array)], {type: 'image/png'});
-        // }
+        function dataURItoBlob(dataURI) {
+          var binary = atob(dataURI.split(',')[1]);
+          var array = [];
+          for(var i = 0; i < binary.length; i++) {
+            array.push(binary.charCodeAt(i));
+          }
+          return new Blob([new Uint8Array(array)], {type: 'image/png'});
+        }
 
-        // var dataUrl = canvas.toDataURL("image/png");
-        // var blobData = dataURItoBlob(dataUrl);
+        var dataUrl = canvas.toDataURL("image/png");
+        var blobData = dataURItoBlob(dataUrl);
 
-        // SockFactory.getUnsignedURL()
-        //   .then(function(res){
-        //     var imageUrl = res.url.split('?')[0];
+        SockFactory.getUnsignedURL()
+          .then(function(res){
+            var imageUrl = res.url.split('?')[0];
 
-        //     $http.put(res.url, blobData,
-        //       {headers: {
-        //       'Content-Type': 'image/png',
-        //         Key : 'ani_ben.png'
-        //     }})
-        //       .then(function(res){
-        //         newSockDataObj.image = imageUrl;
-        //         SockFactory.saveDesign(newSockDataObj)
-        //         .then(function (result) {
-        //           $state.go('user', {userId: result.data.userId})
-        //         })
-        //       })
-        //   })
+            $http.put(res.url, blobData,
+              {headers: {
+              'Content-Type': 'image/png',
+                Key : 'ani_ben.png'
+            }})
+              .then(function(res){
+                newSockDataObj.image = imageUrl;
+                SockFactory.saveDesign(newSockDataObj)
+                .then(function (result) {
+                  $state.go('user', {userId: result.data.userId})
+                })
+              })
+          })
 			 };
 
 
