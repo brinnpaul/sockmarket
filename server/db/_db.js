92fbd4fb1e1268ba53c46d0ef63dbe8bd47865b1
Ani <aganita21@gmail.com>
2016-09-29 02:21:24 -0400

    to deploy to heroku

--- server/db/_db.js
+++ server/db/_db.js
@@ -3,12 +3,12 @@ var Sequelize = require('sequelize');
 
 var env = require(path.join(__dirname, '../env'));
 
-// var db = new Sequelize(env.DATABASE_URI);
+ var db = new Sequelize(env.DATABASE_URI);
 
-var db = new Sequelize('fsg', 'bpr', 'sunshine', {
-dialect: 'postgres',
-port: 5432
-})
+// var db = new Sequelize('fsg', 'bpr', 'sunshine', {
+// dialect: 'postgres',
+// port: 5432
+// })
 
 //var db = new Sequelize('fsg', 'ani', 'sunshine', {
 //  dialect: 'postgres',
